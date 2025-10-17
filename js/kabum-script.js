cce2b1d3385ed005da0fa5ac193c19ee7778ad62
Mateus Granzotto <165953733+MateusFGC@users.noreply.github.com>
2025-10-17 18:18:16 -0300

    Add files via upload

---
+++ js/kabum-script.js
@@ -0,0 +1,38 @@
+
+    var arrowReview = document.querySelector('.kabum-review>svg')
+    var mainVideo = document.getElementById('mainVideo')
+
+    function selectHidden() {
+
+        mainVideo.classList.add('kabum-hidden')
+
+        if (arrowReview.dataset.state == "close") {
+            arrowReview.setAttribute('data-state', 'open')
+            /* mainVideo.kabum-hidden = tru */
+            mainVideo.classList.add('kabum-hidden')
+
+        } else if (arrowReview.dataset.state == "open") {
+            arrowReview.setAttribute('data-state', 'close')
+            /* mainVideo.kabum-hidden = false */
+
+            mainVideo.classList.remove('kabum-hidden')
+        }
+
+
+    }
+    document.querySelector('#kabum-button > button').addEventListener('click', selectHidden)
+
+    function showVideo(event) {
+        var clickedImage = event.currentTarget;
+        var wrapper = clickedImage.closest('.kabum-thumbnail-wrapper');
+        var panel = clickedImage.closest('.uk-panel');
+        var iframe = panel.querySelector('.kabum-video-iframe');
+        wrapper.classList.add('kabum-hidden');
+        iframe.style.display = 'block';
+    }
+
+    document.querySelectorAll('.uk-panel .kabum-thumbnail-wrapper').forEach(function (thumb) {
+        thumb.addEventListener('click', showVideo);
+    });
+
+
