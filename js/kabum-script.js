baf9a84e8218accc85f4d82bca88f655c4bc4cf4
Mateus Granzotto <mateus_granzoto@hotmail.com>
2025-10-17 18:51:55 -0300

    Refatore HTML, CSS e JS para a seção de revisão de vídeo
    Formatação e comentários limpos em HTML e CSS para melhor manutenção e clareza. Estrutura e legibilidade do código aprimoradas em kabum-script.js, removendo recuos desnecessários e simplificando a lógica. Comentários instrucionais adicionados e áreas de personalização esclarecidas no HTML.

--- js/kabum-script.js
+++ js/kabum-script.js
@@ -1,38 +1,35 @@
+var arrowReview = document.querySelector('.kabum-review>svg')
+var mainVideo = document.getElementById('mainVideo')
 
-    var arrowReview = document.querySelector('.kabum-review>svg')
-    var mainVideo = document.getElementById('mainVideo')
+function selectHidden() {
 
-    function selectHidden() {
+    mainVideo.classList.add('kabum-hidden')
 
+    if (arrowReview.dataset.state == "close") {
+        arrowReview.setAttribute('data-state', 'open')
+        /* mainVideo.kabum-hidden = tru */
         mainVideo.classList.add('kabum-hidden')
 
-        if (arrowReview.dataset.state == "close") {
-            arrowReview.setAttribute('data-state', 'open')
-            /* mainVideo.kabum-hidden = tru */
-            mainVideo.classList.add('kabum-hidden')
+    } else if (arrowReview.dataset.state == "open") {
+        arrowReview.setAttribute('data-state', 'close')
+        /* mainVideo.kabum-hidden = false */
 
-        } else if (arrowReview.dataset.state == "open") {
-            arrowReview.setAttribute('data-state', 'close')
-            /* mainVideo.kabum-hidden = false */
-
-            mainVideo.classList.remove('kabum-hidden')
-        }
-
-
-    }
-    document.querySelector('#kabum-button > button').addEventListener('click', selectHidden)
-
-    function showVideo(event) {
-        var clickedImage = event.currentTarget;
-        var wrapper = clickedImage.closest('.kabum-thumbnail-wrapper');
-        var panel = clickedImage.closest('.uk-panel');
-        var iframe = panel.querySelector('.kabum-video-iframe');
-        wrapper.classList.add('kabum-hidden');
-        iframe.style.display = 'block';
+        mainVideo.classList.remove('kabum-hidden')
     }
 
-    document.querySelectorAll('.uk-panel .kabum-thumbnail-wrapper').forEach(function (thumb) {
-        thumb.addEventListener('click', showVideo);
-    });
 
+}
+document.querySelector('#kabum-button > button').addEventListener('click', selectHidden)
 
+function showVideo(event) {
+    var clickedImage = event.currentTarget;
+    var wrapper = clickedImage.closest('.kabum-thumbnail-wrapper');
+    var panel = clickedImage.closest('.uk-panel');
+    var iframe = panel.querySelector('.kabum-video-iframe');
+    wrapper.classList.add('kabum-hidden');
+    iframe.style.display = 'block';
+}
+
+document.querySelectorAll('.uk-panel .kabum-thumbnail-wrapper').forEach(function (thumb) {
+    thumb.addEventListener('click', showVideo);
+});
